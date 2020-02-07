003d35ec698668cfdfff8bd999895b0702b22345
Steffen Keuper <steffen.keuper@web.de>
2020-02-07 16:41:28 +0100

    [TASK] use clap 3.0.0 beta version from github, add example application in the v3 style

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,71 @@
+// (Full example with detailed comments in examples/01d_quick_example.rs)
+//
+// This example demonstrates clap's full 'custom derive' style of creating arguments which is the
+// simplest method of use, but sacrifices some flexibility.
+use clap::Clap;
 use rusqlite::Error;
 
 mod database;
 
-fn main() -> Result<(), Error> {
-    let con = database::Database::open("figure_tracker.db")?;
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Kevin K.")]
+struct Opts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short = "c", long = "config", default_value = "default.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: i32,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
 
-    Ok(())
+#[derive(Clap)]
+enum SubCommand {
+    /// A help message for the Test subcommand
+    #[clap(name = "test", version = "1.3", author = "Someone Else")]
+    Test(Test),
+}
+
+/// A subcommand for controlling testing
+#[derive(Clap)]
+struct Test {
+    /// Print debug info
+    #[clap(short = "d")]
+    debug: bool
+}
+
+fn main() {
+    let _con = database::Database::open("figure_tracker.db");
+
+    let opts: Opts = Opts::parse();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input);
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Test(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
 }

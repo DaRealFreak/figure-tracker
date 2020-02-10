9fbb8684ef15f51f9c45b88e748cc97bc14e4db9
DaRealFreak <steffen.keuper@web.de>
2020-02-10 02:36:34 +0100

    [TASK] add yaml_rust to crates, add default configuration yaml, parse configuration from yaml and use for database path

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ extern crate chrono;
 extern crate env_logger;
 #[macro_use]
 extern crate log;
+extern crate yaml_rust;
 
 use std::error::Error;
+use std::fs::{File, read_to_string};
+use std::io;
 use std::io::Write;
+use std::path::Path;
 use std::process;
 
 use chrono::Local;
 use clap::Clap;
 use env_logger::Builder;
 use log::LevelFilter;
+use yaml_rust::{Yaml, YamlLoader};
 
 use crate::database::Database;
 
@@ -20,6 +25,7 @@ mod database;
 struct FigureTracker {
     options: FigureTrackerOptions,
     db: Option<Database>,
+    config: Option<Yaml>,
 }
 
 /// This application tracks wished items on multiple seller/auction sites
@@ -67,6 +73,31 @@ struct AddItem {
 
 /// main implementation of the figure tracker
 impl FigureTracker {
+    /// main entry point, here the CLI options are parsed
+    pub fn execute(&mut self) {
+        self.initialize_logger();
+
+        if let Err(err) = self.parse_configuration() {
+            error!("couldn't parse or create the configuration (err: {})", err.description());
+            process::exit(1)
+        };
+
+        self.open_database();
+
+        match &self.options.subcmd {
+            SubCommand::Add(t) => {
+                match &t.subcmd {
+                    AddItemSubCommand::AddItem(item) => {
+                        info!("adding item to the database: {:?}", item.input);
+                    }
+                }
+            }
+            _ => {
+                info!("test")
+            }
+        }
+    }
+
     /// initializes the logger across the project
     pub fn initialize_logger(&self) {
         // Gets a value for config if supplied by user, or defaults to "tracker.yaml"
@@ -95,8 +126,28 @@ impl FigureTracker {
         logger.init();
     }
 
+    /// parses the passed/default configuration file or creates it if it doesn't exist yet
+    pub fn parse_configuration(&mut self) -> Result<(), io::Error> {
+        if !Path::new(self.options.config.as_str()).exists() {
+            let bytes = include_bytes!("../default.yaml");
+
+            let mut file = File::create(self.options.config.as_str())?;
+            file.write_all(bytes)?;
+        }
+
+        let config_content = read_to_string(self.options.config.as_str())?;
+        self.config = Option::from(YamlLoader::load_from_str(config_content.as_str()).unwrap()[0].clone());
+
+        Ok(())
+    }
+
     /// open or create the requested SQLite database file, exits if an error occurred
     pub fn open_database(&mut self) {
+        if self.config.as_ref().unwrap()["database"]["path"].is_badvalue() {
+            error!("path to the database file is not defined");
+            process::exit(1)
+        }
+
         let db = database::Database::open("figure_tracker.db");
         if let Err(db) = db {
             error!("couldn't open database (err: {})", db.description());
@@ -105,30 +156,12 @@ impl FigureTracker {
 
         self.db = Option::from(db.unwrap());
     }
-
-    /// main entry point, here the CLI options are parsed
-    pub fn execute(&mut self) {
-        self.initialize_logger();
-        self.open_database();
-
-        match &self.options.subcmd {
-            SubCommand::Add(t) => {
-                match &t.subcmd {
-                    AddItemSubCommand::AddItem(item) => {
-                        info!("adding item to the database: {:?}", item.input);
-                    }
-                }
-            }
-            _ => {
-                info!("test")
-            }
-        }
-    }
 }
 
 fn main() {
     FigureTracker {
         options: FigureTrackerOptions::parse(),
         db: None,
+        config: None,
     }.execute();
 }

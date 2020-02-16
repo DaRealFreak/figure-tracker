cb86a439e221b94b1c1da378903fcfdb1e4f9015
DaRealFreak <steffen.keuper@web.de>
2020-02-16 03:40:34 +0100

    [TASK] add an item condition and add an additional function to retrieve all tracked items

--- src/database/items.rs
+++ src/database/items.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
-use rusqlite::params;
+use rusqlite::types::{FromSql, FromSqlError, FromSqlResult, ToSqlOutput, ValueRef};
+use rusqlite::{params, ToSql};
 
 use crate::database::Database;
 
@@ -13,8 +14,39 @@ pub(crate) struct Item {
     pub(crate) disabled: bool,
 }
 
+/// Available item conditions to request for
+#[derive(Clone)]
+pub(crate) enum ItemConditions {
+    New,
+    Used,
+}
+
+/// implementation for the ToSql trait for the rusqlite dependency
+/// to only allow specific item conditions
+impl ToSql for ItemConditions {
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        match self {
+            ItemConditions::New => Ok(ToSqlOutput::from("new")),
+            ItemConditions::Used => Ok(ToSqlOutput::from("used")),
+        }
+    }
+}
+
+/// implementation for the FromSql trait for the rusqlite dependency
+/// to only allow specific item conditions
+impl FromSql for ItemConditions {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        value.as_str().and_then(|s| match s {
+            "new" => Ok(ItemConditions::New),
+            "used" => Ok(ItemConditions::Used),
+            _ => Err(FromSqlError::InvalidType),
+        })
+    }
+}
+
 /// Items implements all related functionality for items to interact with the database
 pub(crate) trait Items {
+    fn get_items(&self) -> Result<Vec<Item>, Box<dyn Error>>;
     fn get_item(&self, jan: &i64) -> Result<Item, Box<dyn Error>>;
     fn add_item(&self, jan: &i64) -> Result<Item, Box<dyn Error>>;
     fn update_item(&self, item: Item) -> Result<(), Box<dyn Error>>;
@@ -22,6 +54,31 @@ pub(crate) trait Items {
 
 /// Items is the implementation of the Items trait
 impl Items for Database {
+    fn get_items(&self) -> Result<Vec<Item>, Box<dyn Error>> {
+        let mut stmt = self.conn.prepare(
+            "SELECT id, jan, term, description, disabled
+             FROM tracked_items
+             WHERE disabled = 0",
+        )?;
+
+        let res = stmt.query_map(params![], |row| {
+            Ok(Item {
+                id: row.get(0)?,
+                jan: row.get(1)?,
+                term: row.get(2)?,
+                description: row.get(3)?,
+                disabled: row.get(4)?,
+            })
+        })?;
+
+        let mut items: Vec<Item> = vec![];
+        for item in res {
+            items.push(item.unwrap())
+        }
+
+        Ok(items)
+    }
+
     /// retrieve an item from the database based on their JAN number
     fn get_item(&self, jan: &i64) -> Result<Item, Box<dyn Error>> {
         let mut stmt = self.conn.prepare(

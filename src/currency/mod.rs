c0c774bbce61620300722b9cf5f48bf18cc842af
Steffen Keuper <steffen.keuper@web.de>
2020-02-20 17:19:06 +0100

    [TASK] implement retrieval of the currency rates of the ECB and implement conversion

--- src/currency/mod.rs
+++ src/currency/mod.rs
@@ -4,6 +4,7 @@ use std::fmt;
 use std::fmt::Display;
 
 use ordered_float::OrderedFloat;
+use serde::Deserialize;
 use strsim::normalized_levenshtein;
 
 /// struct to store our supported currencies retrieved from the ECB
@@ -172,33 +173,106 @@ impl CurrencyGuesser {
     }
 }
 
+/// structs used for the deserialization of the EUR reference values of the ECB
+#[derive(Deserialize)]
+struct Envelope {
+    #[serde(rename = "Cube")]
+    pub cube: Cube,
+}
+
+#[derive(Deserialize)]
+struct Cube {
+    #[serde(rename = "Cube")]
+    pub time: CubeTime,
+}
+
+#[derive(Deserialize)]
+struct CubeTime {
+    #[serde(rename = "time")]
+    pub date: String,
+    #[serde(rename = "Cube")]
+    pub currencies: Vec<CubeCurrency>,
+}
+
+#[derive(Deserialize)]
+struct CubeCurrency {
+    pub currency: String,
+    pub rate: String,
+}
+
 /// struct to use most current exchange rates to convert currencies to one equal currency
-pub(crate) struct CurrencyConversion {}
+pub(crate) struct CurrencyConversion {
+    exchange_rates: BTreeMap<SupportedCurrency, f64>,
+}
 
 ///implementation for the currency conversion
 impl CurrencyConversion {
     /// retrieve instance of currency conversion with initialized currencies and exchange rates
-    pub fn new() -> Self {
-        CurrencyConversion {}
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(CurrencyConversion {
+            exchange_rates: CurrencyConversion::get_conversion_rates()?,
+        })
     }
 
-    /// update the conversion rates using the ECB
-    pub fn update_conversion_rates() -> Result<(), Box<dyn Error>> {
-        // ToDo: use https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
-        // to retrieve most current exchange rates (EUR = 1.0) and save them
-        Ok(())
+    /// update the conversion rates using the ECB euro reference
+    fn get_conversion_rates() -> Result<BTreeMap<SupportedCurrency, f64>, Box<dyn Error>> {
+        let mut exchange_rates: BTreeMap<SupportedCurrency, f64> = BTreeMap::new();
+        // insert the base currency here which is not in the exchange information
+        exchange_rates.insert(SupportedCurrency::EUR, 1.00);
+
+        let res = reqwest::blocking::get(
+            "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
+        )?;
+        let envelope: Envelope = quick_xml::de::from_str(&res.text()?).unwrap();
+        let currency_guesser = CurrencyGuesser::new();
+
+        info!(
+            "extracted exchange rates from date: {:?}",
+            envelope.cube.time.date
+        );
+
+        for currency in envelope.cube.time.currencies.iter() {
+            let exchange_rate: f64 = currency.rate.parse()?;
+            if let Some(guessed_currency) = currency_guesser
+                .guess_currency_from_code(currency.currency.as_str().to_string(), true)
+            {
+                exchange_rates.insert(guessed_currency.clone(), exchange_rate);
+            }
+        }
+
+        Ok(exchange_rates)
     }
 
     /// convert the passed value to the equivalent of the passed to currency
-    pub fn convert_price_to(value: f64, from: &SupportedCurrency, to: &SupportedCurrency) -> f64 {
-        // ToDo: use the currently not implemented yet exchange rates to calculate the proper
-        // value of the passed value
-        0.0
+    pub fn convert_price_to(
+        &self,
+        value: f64,
+        from: SupportedCurrency,
+        to: SupportedCurrency,
+    ) -> f64 {
+        value / *self.exchange_rates.get(&from).unwrap() * *self.exchange_rates.get(&to).unwrap()
     }
 }
 
 #[test]
 fn test_currency_conversion() {
+    match CurrencyConversion::new() {
+        Ok(converter) => {
+            println!(
+                "$150 are currently {}€",
+                converter.convert_price_to(150.0, SupportedCurrency::USD, SupportedCurrency::EUR)
+            );
+            println!(
+                "150€ are currently ${}",
+                converter.convert_price_to(150.0, SupportedCurrency::EUR, SupportedCurrency::USD)
+            );
+        }
+        Err(_) => assert!(false),
+    }
+}
+
+#[test]
+fn test_currency_guesses() {
     let guesser = CurrencyGuesser::new();
 
     let test_values: Vec<(&str, Option<&SupportedCurrency>)> = vec![

a3dbf304d62d8a2c358654719710dcb6453122a6
Steffen Keuper <steffen.keuper@web.de>
2020-03-06 17:27:51 +0100

    [TASK] rename Conditions to ConditionType, derive it additionally from Clone and Copy and implement ToString for it

--- src/conditions/mod.rs
+++ src/conditions/mod.rs
@@ -1,5 +1,5 @@
-#[derive(Debug)]
-pub(crate) enum Conditions {
+#[derive(Clone, Copy, Debug)]
+pub(crate) enum ConditionType {
     BelowPrice,
     BelowPriceTaxed,
     BelowPriceFull,
@@ -18,16 +18,28 @@ impl ToString for InvalidConditionError {
     }
 }
 
-impl std::str::FromStr for Conditions {
+impl ToString for ConditionType {
+    fn to_string(&self) -> String {
+        match self {
+            ConditionType::BelowPrice => "below_price".to_string(),
+            ConditionType::BelowPriceTaxed => "below_price_taxed".to_string(),
+            ConditionType::BelowPriceFull => "below_price_full".to_string(),
+            ConditionType::LowestPrice => "lowest_price".to_string(),
+            ConditionType::PriceDrop => "price_drop".to_string(),
+        }
+    }
+}
+
+impl std::str::FromStr for ConditionType {
     type Err = InvalidConditionError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "below_price" => Ok(Conditions::BelowPrice),
-            "below_price_taxed" => Ok(Conditions::BelowPriceTaxed),
-            "below_price_full" => Ok(Conditions::BelowPriceFull),
-            "lowest_price" => Ok(Conditions::LowestPrice),
-            "price_drop" => Ok(Conditions::PriceDrop),
+            "below_price" => Ok(ConditionType::BelowPrice),
+            "below_price_taxed" => Ok(ConditionType::BelowPriceTaxed),
+            "below_price_full" => Ok(ConditionType::BelowPriceFull),
+            "lowest_price" => Ok(ConditionType::LowestPrice),
+            "price_drop" => Ok(ConditionType::PriceDrop),
             _ => Err(InvalidConditionError {
                 msg: format!(
                     "{:?} is not a valid condition type, add --help to see the valid options",

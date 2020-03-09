254b0fbc3c35b6381815230db8c954029c56a214
DaRealFreak <steffen.keuper@web.de>
2020-03-09 02:57:51 +0100

    [TASK] add functionality to check for lowest prices of item and of item before passed price, add functionality to check for condition

--- src/database/prices.rs
+++ src/database/prices.rs
@@ -1,9 +1,13 @@
+use core::fmt;
 use std::error::Error;
 
 use chrono::{DateTime, Utc};
 use rusqlite::params;
+use serde::export::Formatter;
 
+use crate::conditions::ConditionType;
 use crate::currency::SupportedCurrency;
+use crate::database::conditions::Condition;
 use crate::database::items::{Item, ItemConditions};
 use crate::database::Database;
 
@@ -65,7 +69,32 @@ impl Price {
 /// Prices implements all related functionality for prices to interact with the database
 pub(crate) trait Prices {
     fn add_price(&self, price: &Price) -> Result<(), Box<dyn Error>>;
+    fn get_lowest_price_by_item_id(&self, item_id: i64) -> Result<Option<Price>, Box<dyn Error>>;
+    fn get_lowest_price_before_price(&self, price: Price) -> Result<Option<Price>, Box<dyn Error>>;
+    fn matches_condition(&self, price: Price, condition: Condition) -> bool;
 }
+
+struct NoPriceFoundError {}
+
+impl NoPriceFoundError {
+    fn display(&self) -> String {
+        "no related price was found in the database".to_string()
+    }
+}
+
+impl std::fmt::Display for NoPriceFoundError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.display())
+    }
+}
+
+impl std::fmt::Debug for NoPriceFoundError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.display())
+    }
+}
+
+impl std::error::Error for NoPriceFoundError {}
 
 /// Prices is the implementation of the Prices trait
 impl Prices for Database {
@@ -91,5 +120,116 @@ impl Prices for Database {
         )?;
 
         Ok(())
+    }
+
+    fn get_lowest_price_by_item_id(&self, item_id: i64) -> Result<Option<Price>, Box<dyn Error>> {
+        let mut stmt = self.conn.prepare(
+            "SELECT id, item_id, price, currency, converted_price, converted_currency, taxes,
+                    shipping, url, module, condition, tstamp
+            FROM prices
+            WHERE item_id = ?1
+            GROUP BY item_id, tstamp
+            ORDER BY converted_price
+            LIMIT 1",
+        )?;
+
+        let mut price_iter = stmt.query_map(params![item_id], |row| {
+            Ok(Price {
+                id: row.get(0)?,
+                item_id: row.get(1)?,
+                price: row.get(2)?,
+                currency: row.get(3)?,
+                converted_price: row.get(4)?,
+                converted_currency: row.get(5)?,
+                taxes: row.get(6)?,
+                shipping: row.get(7)?,
+                url: row.get(8)?,
+                module: row.get(9)?,
+                condition: row.get(10)?,
+                timestamp: row.get(11)?,
+            })
+        })?;
+
+        match price_iter.next() {
+            Some(price) => Ok(Some(price.unwrap())),
+            None => Err(Box::from(NoPriceFoundError {})),
+        }
+    }
+
+    fn get_lowest_price_before_price(&self, price: Price) -> Result<Option<Price>, Box<dyn Error>> {
+        let mut stmt = self.conn.prepare(
+            "SELECT id, item_id, price, currency, converted_price, converted_currency, taxes,
+                    shipping, url, module, condition, tstamp
+            FROM prices
+            WHERE item_id = ?1
+                AND tstamp < ?2
+            GROUP BY item_id, tstamp
+            ORDER BY converted_price
+            LIMIT 1",
+        )?;
+
+        let mut price_iter = stmt.query_map(params![price.item_id, price.timestamp], |row| {
+            Ok(Price {
+                id: row.get(0)?,
+                item_id: row.get(1)?,
+                price: row.get(2)?,
+                currency: row.get(3)?,
+                converted_price: row.get(4)?,
+                converted_currency: row.get(5)?,
+                taxes: row.get(6)?,
+                shipping: row.get(7)?,
+                url: row.get(8)?,
+                module: row.get(9)?,
+                condition: row.get(10)?,
+                timestamp: row.get(11)?,
+            })
+        })?;
+
+        match price_iter.next() {
+            Some(price) => Ok(Some(price.unwrap())),
+            None => Err(Box::from(NoPriceFoundError {})),
+        }
+    }
+
+    /// check if the passed price matches the passed condition and should notify the user
+    /// about the price
+    fn matches_condition(&self, price: Price, condition: Condition) -> bool {
+        match condition.condition_type {
+            /// below static value of the condition
+            ConditionType::BelowPrice => price.converted_price < condition.value,
+            /// below taxed value of the condition
+            ConditionType::BelowPriceTaxed => price.get_converted_taxed() < condition.value,
+            /// below the full value of the condition
+            ConditionType::BelowPriceFull => price.get_converted_total() < condition.value,
+            /// retrieve the lowest item so far
+            /// and check if the current item is x amount below the value
+            ConditionType::LowestPrice => {
+                if let Ok(price_option) = self.get_lowest_price_by_item_id(price.item_id) {
+                    match price_option {
+                        Some(lowest_price) => {
+                            price.converted_price + condition.value < lowest_price.converted_price
+                        }
+                        None => true,
+                    }
+                } else {
+                    false
+                }
+            }
+            /// retrieve the lowest item recorded before the current item
+            /// and check if the price dropped by x percentage compared to it
+            ConditionType::PriceDrop => {
+                if let Ok(price_option) = self.get_lowest_price_before_price(price.clone()) {
+                    match price_option {
+                        Some(previous_price) => {
+                            previous_price.converted_price * (1.0 - condition.value)
+                                > price.converted_price
+                        }
+                        None => false,
+                    }
+                } else {
+                    false
+                }
+            }
+        }
     }
 }

db356d03cec56c70b258e7c2fe305624e8fa58cf
DaRealFreak <steffen.keuper@web.de>
2020-03-10 02:32:46 +0100

    [TASK] implement notification manager to manage different notification types, implement Notification trait for Telegram

--- src/notifications/mod.rs
+++ src/notifications/mod.rs
@@ -1,1 +1,41 @@
+use std::error::Error;
+
+use crate::configuration::Configuration;
+use crate::database::conditions::Condition;
+use crate::database::items::Item;
+use crate::database::prices::Price;
+use crate::notifications::telegram::{Telegram, TelegramApiData};
+
 pub(crate) mod telegram;
+
+pub(crate) struct NotificationManager {
+    telegram_data: Option<TelegramApiData>,
+}
+
+pub(crate) trait Notification {
+    fn notify(&self, item: Item, price: Price, cond: Condition) -> Result<(), Box<dyn Error>>;
+}
+
+impl NotificationManager {
+    pub fn new() -> Self {
+        let mut manager = NotificationManager {
+            telegram_data: None,
+        };
+
+        if let Some(api_key) = Configuration::get_telegram_api_key() {
+            if let Some(user_id) = Configuration::get_telegram_user_id() {
+                manager.telegram_data = Some(TelegramApiData::new(api_key, user_id))
+            }
+        }
+
+        manager
+    }
+
+    pub fn notify(&self, item: Item, price: Price, cond: Condition) -> Result<(), Box<dyn Error>> {
+        if let Some(data) = self.telegram_data.as_ref() {
+            Telegram::new(data.clone()).notify(item, price, cond)?;
+        }
+
+        Ok(())
+    }
+}
--- src/notifications/telegram/mod.rs
+++ src/notifications/telegram/mod.rs
@@ -1,47 +1,102 @@
 use std::error::Error;
 
-use telegram_bot::{Api, FileRef, InputFile, SendMessage, SendPhoto, UserId};
+use telegram_bot::{Api, FileRef, InputFile, ParseMode, SendMessage, SendPhoto, UserId};
 use tokio::runtime::Runtime;
 
-struct Telegram {
-    core: Runtime,
+use crate::database::conditions::Condition;
+use crate::database::items::Item;
+use crate::database::prices::Price;
+use crate::notifications::Notification;
+
+#[derive(Clone)]
+pub(crate) struct TelegramApiData {
+    api_key: String,
+    user_id: i64,
+}
+
+impl TelegramApiData {
+    pub fn new(api_key: String, user_id: i64) -> Self {
+        TelegramApiData { api_key, user_id }
+    }
+}
+
+pub(crate) struct Telegram {
     api: Api,
+    user_id: i64,
 }
 
 impl Telegram {
     /// return instance of the Telegram Notifications which acts as a wrapper for the telegram-bot library
     /// it also handles the future promises and you can use it in a blocking context
-    pub fn new(api_key: &str) -> Telegram {
+    pub fn new(data: TelegramApiData) -> Telegram {
         Telegram {
-            core: Runtime::new().unwrap(),
-            api: Api::new(api_key.to_string()),
+            api: Api::new(data.api_key),
+            user_id: data.user_id,
         }
     }
 
     /// send the passed message to the passed user ID
-    pub fn send_message(&mut self, user_id: i64, message: &str) -> Result<(), Box<dyn Error>> {
-        let send_message = SendMessage::new(UserId::from(user_id), message);
-        self.core.block_on(self.api.send(send_message))?;
+    fn send_message(&self, message: String) -> Result<(), Box<dyn Error>> {
+        let mut send_message = SendMessage::new(UserId::from(self.user_id), message);
+        send_message.parse_mode(ParseMode::Html);
+        Runtime::new()
+            .unwrap()
+            .block_on(self.api.send(send_message))?;
         Ok(())
     }
 
     /// send the passed image with the optional passed caption to the passed user ID
     /// the image path must be available online though and can't be an local file
-    pub fn send_image_message(
-        &mut self,
-        user_id: i64,
-        image_url: &str,
-        message: Option<&str>,
-    ) -> Result<(), Box<dyn Error>> {
+    fn send_image_message(&self, image_url: String, message: String) -> Result<(), Box<dyn Error>> {
         let mut send_image = SendPhoto::new(
-            UserId::from(user_id),
+            UserId::from(self.user_id),
             InputFile::from(FileRef::from(image_url)),
         );
-        if let Some(message) = message {
-            send_image.caption(message);
+        send_image.caption(message);
+
+        Runtime::new()
+            .unwrap()
+            .block_on(self.api.send(send_image))?;
+        Ok(())
+    }
+}
+
+impl Notification for Telegram {
+    fn notify(&self, item: Item, price: Price, cond: Condition) -> Result<(), Box<dyn Error>> {
+        let msg = format!(
+            "met search notification on item:\n\
+             <b>{:?}</b>\n\
+             \n\
+             price: <b>{:.2} {}</b>\n\
+             price with taxes: <b>{:.2} {}</b>\n\
+             price with shipping and taxes: <b>{:.2} {}</b>\n\
+             raw price: <b>{:.2} {}</b>\n\
+             \n\
+             notification type: <b>{:?}</b>\n\
+             item condition: <b>{:?}</b>\n\
+             value: <b>{:.2}</b>\n\
+             \n\
+             link: {}",
+            item.description,
+            price.converted_price,
+            price.converted_currency,
+            price.get_converted_taxed(),
+            price.converted_currency,
+            price.get_converted_total(),
+            price.converted_currency,
+            price.price,
+            price.currency,
+            cond.condition_type,
+            cond.item_condition,
+            cond.value,
+            price.url,
+        );
+        if !item.image.is_empty() {
+            self.send_image_message(item.image, msg)?;
+        } else {
+            self.send_message(msg)?;
         }
 
-        self.core.block_on(self.api.send(send_image))?;
         Ok(())
     }
 }
